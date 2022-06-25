b508d6cdce827569b194c24fad7c0dbabd7e4060
Debumax <deboramarcial1991@gmail.com>
2022-06-25 03:09:00 -0300

    listando archivos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("haciendo la wea esta ");
+
+
+string rutaCarpeta=@"..\tp08-2022-Debumax";
+
+List<string> listaDeContenido=Directory.GetFiles(rutaCarpeta).ToList();
+Console.WriteLine($"la ruta es : {rutaCarpeta}");
+foreach (string archivos in listaDeContenido)
+{
+ 
+    Console.WriteLine(archivos);
+}
+
+string archivo = rutaCarpeta+@"\index.csv";
+if (!File.Exists(archivo))
+{
+    File.Create(archivo);
+}
